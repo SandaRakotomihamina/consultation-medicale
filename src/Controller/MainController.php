7140d832dc78684280c80ca196c765d5f5bbbf3d
SandaRakotomihamina <rakotomihaminasandafitia@gmail.com>
2025-11-14 19:52:38 +0300

    Deuxieme commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,8 @@ use App\Form\ConsultationType;
 use App\Form\UserType;
 use App\Repository\ConsultationListRepository;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class MainController extends AbstractController
 {
@@ -25,15 +28,15 @@ final class MainController extends AbstractController
             'consultations' => $consultations,
         ]);
     }
-
     #[Route('/consultation/new', name: 'app_new_consultation')]
+    #[IsGranted('ROLE_USER')]
+    #[IsGranted(!'ROLE_ADMIN')]
+
     public function newConsultation(Request $request, EntityManagerInterface $em): Response
     {
         $consultation = new ConsultationList();
-        // Mettre la date de consultation automatiquement à maintenant (heure du serveur)
         $consultation->setDate(new \DateTime());
 
-        // Pre-remplir par le nom de l'utilisateur connecté dans le champ "Délivreur d'observation"
         $user = $this->getUser();
         if ($user instanceof User) {
             $delivreur = trim($user->getTitle() . ' ' . $user->getName());
@@ -41,7 +44,6 @@ final class MainController extends AbstractController
         }
 
         $form = $this->createForm(ConsultationType::class, $consultation);
-        // enlever la date du formulaire car elle est mis automatiquement
         if ($form->has('Date')) {
             $form->remove('Date');
         }
@@ -52,7 +54,6 @@ final class MainController extends AbstractController
             $em->flush();
 
             $this->addFlash('success', 'Consultation enregistrée avec succès.');
-
             return $this->redirectToRoute('app_main');
         }
 
@@ -61,7 +62,9 @@ final class MainController extends AbstractController
         ]);
     }
 
+    // --- ROLE_ADMIN SEULEMENT ---
     #[Route('/user/new', name: 'app_new_user')]
+    #[IsGranted('ROLE_ADMIN')]
     public function newUser(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
@@ -76,19 +79,69 @@ final class MainController extends AbstractController
                 $user->setPassword($hashed);
             }
 
-            // Default role
             $user->setRoles(['ROLE_USER']);
 
             $em->persist($user);
             $em->flush();
 
             $this->addFlash('success', 'Utilisateur créé.');
-
             return $this->redirectToRoute('app_main');
         }
 
         return $this->render('main/new_user.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/search', name: 'app_search')]
+    public function search(Request $request, ConsultationListRepository $repo): Response
+    {
+        $query = $request->query->get('q', '');
+
+        $results = [];
+        if ($query) {
+            $results = $repo->createQueryBuilder('c')
+                ->where('c.Nom LIKE :q OR c.Matricule LIKE :q')
+                ->setParameter('q', "%$query%")
+                ->getQuery()
+                ->getResult();
+        }
+
+        return $this->render('main/search_results.html.twig', [
+            'results' => $results,
+            'query' => $query,
+        ]);
+    }
+
+    #[Route('/consultation/edit/{id}', name: 'app_consultation_edit')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(ManagerRegistry $doctrine, Request $request, string $id): Response
+    {
+        $em = $doctrine->getManager();
+        $consultation = $doctrine->getRepository(ConsultationList::class)->find($id);
+
+        if (!$consultation) {
+            throw $this->createNotFoundException('Consultation non trouvée pour l\'id '.$id);
+        }
+
+        if ($request->isMethod('POST')) {
+            $consultation->setGrade($request->request->get('grade'));
+            $consultation->setNom($request->request->get('nom'));
+            $consultation->setMatricule($request->request->get('matricule'));
+            $consultation->setMotif($request->request->get('motif'));
+            $consultation->setDelivreurDeMotif($request->request->get('delivreur_de_motif'));
+            $consultation->setObservation($request->request->get('observation'));
+            $consultation->setDelivreurDObservation($request->request->get('delivreur_d_observation'));
+
+            $em->persist($consultation);
+            $em->flush();
+
+            $this->addFlash('success', 'Consultation mise à jour avec succès !');
+            return $this->redirectToRoute('app_main');
+        }
+
+        return $this->render('main/edit_consultation.html.twig', [
+            'consultation' => $consultation
         ]);
     }
 }
