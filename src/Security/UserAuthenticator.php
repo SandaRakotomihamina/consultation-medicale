9ca2d6447f500d9fff6ab3a5562c63150e049cad
SandaRakotomihamina <rakotomihaminasandafitia@gmail.com>
2025-12-05 08:31:25 +0300

    53e commit

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -2,11 +2,13 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -21,8 +23,10 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
-    {
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private UserRepository $userRepository
+    ) {
     }
 
     public function authenticate(Request $request): Passport
@@ -32,7 +36,24 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $username);
 
         return new Passport(
-            new UserBadge($username),
+            new UserBadge(
+                $username,
+                function (string $identifier) {
+                    // Chercher d'abord par username
+                    $user = $this->userRepository->findOneBy(['username' => $identifier]);
+                    
+                    // Si pas trouvé, chercher par matricule
+                    if (!$user) {
+                        $user = $this->userRepository->findOneBy(['Matricule' => $identifier]);
+                    }
+                    
+                    if (!$user) {
+                        throw new UserNotFoundException(sprintf('User "%s" not found.', $identifier));
+                    }
+                    
+                    return $user;
+                }
+            ),
             new PasswordCredentials($request->getPayload()->getString('password')),
             [
                 new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),           
